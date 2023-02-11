3c9dc61d1bafd078536ea98501c6c13fb67dcae9
Anthony Kim <artit.anthony@gmail.com>
2023-02-11 12:30:14 +0900

    update install script (#3)
    Co-authored-by: dong-hyun <dong.hyun@navercorp.com>

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+CHROME_VERSION=109.0.5414.74
+
 sudo apt update
 
 # ETC
@@ -16,18 +18,18 @@ sudo apt-get install -y lmdb-utils
 sudo apt-get install -y gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0
 sudo apt-get install -y libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2
 sudo apt-get install -y libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils
+sudo apt-get install -y libglib2.0 libnss3 libgconf-2-4 libfontconfig1
 
 # Install google-chrome (version 103.0.5060.xxx) on ubuntu.
 # Change version if you want to use up-to-date chrome.
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt-get update
-sudo apt-get install google-chrome-stable=103.0.5060.134-1
+wget http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_$CHROME_VERSION-1_amd64.deb
+sudo dpkg -i google-chrome-stable_$CHROME_VERSION-1_amd64.deb
+rm google-chrome-stable_$CHROME_VERSION-1_amd64.deb
 
 # Download executable google-chrome.
-wget https://chromedriver.storage.googleapis.com/103.0.5060.24/chromedriver_linux64.zip
+wget https://chromedriver.storage.googleapis.com/$CHROME_VERSION/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
-mv chromedriver resources/chromedriver_linux64_103.0.5060.24
+mv chromedriver resources/chromedriver
 rm chromedriver_linux64.zip
 
 # Languages
